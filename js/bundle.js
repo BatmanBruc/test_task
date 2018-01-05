7fa9afb4947af48449cf2c8b9f682c05589367d2
BatBruc <pupkinandroit@gmail.com>
2018-01-05 21:10:32 +1000

    Добавил новый модуль babel

--- js/bundle.js
+++ js/bundle.js
@@ -65,17 +65,14 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sass_style_scss__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sass_style_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__sass_style_scss__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__main_js__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__main_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__main_js__);
 
 
+__webpack_require__(1);
 
+__webpack_require__(2);
 
 /***/ }),
 /* 1 */
@@ -87,7 +84,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* 2 */
 /***/ (function(module, exports) {
 
-console.log('все ок')
+console.log('все ок');
 
 /***/ })
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,16 @@ module.exports = {
           fallback: 'style-loader',
           use: ['css-loader?url=false', 'sass-loader?url=false']
         })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
